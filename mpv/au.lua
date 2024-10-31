9088f9f10c9e62cd9ad1894686604ef9685b7681
shurizzle <me@shurizzle.dev>
2024-10-31 20:32:04 +0100

    feat: add mpv script

---
+++ mpv/au.lua
@@ -0,0 +1,28 @@
+mp.add_hook("on_load", 50, function()
+	local url = mp.get_property("stream-open-filename")
+	if type(url) ~= "string" then
+		return
+	end
+
+	local res, _ = mp.command_native({
+		name = "subprocess",
+		args = { "aumpv", url },
+		playback_only = false,
+		capture_stdout = true,
+	})
+	if not res or res.error or not res.stdout then
+		return
+	end
+	local info = require("mp.utils").parse_json(res.stdout)
+
+	if info.type == "video" then
+		mp.set_property("stream-open-filename", info.url)
+	elseif info.type == "playlist" then
+		if #info.items > 0 then
+			for i = #info.items, 1, -1 do
+				mp.commandv("loadfile", info.items[i], "insert-next")
+			end
+		end
+		mp.commandv("playlist-remove", "current")
+	end
+end)
