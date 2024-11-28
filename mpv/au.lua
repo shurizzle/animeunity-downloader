efcbfe670d2123343ca3dc5a035b732692617a7e
shurizzle <me@shurizzle.dev>
2024-11-28 21:58:36 +0100

    fix: check for json errors

--- mpv/au.lua
+++ mpv/au.lua
@@ -9,16 +9,20 @@ mp.add_hook("on_load", 50, function()
 		args = { "aumpv", url },
 		playback_only = false,
 		capture_stdout = true,
+		capture_stderr = true,
 	})
 	if not res or res.error or not res.stdout then
 		return
 	end
 	local info = require("mp.utils").parse_json(res.stdout)
+	if not info then
+		return
+	end
 
 	if info.type == "video" then
 		mp.set_property("stream-open-filename", info.url)
 	elseif info.type == "playlist" then
-		if #info.items > 0 then
+		if info.items and #info.items > 0 then
 			for i = #info.items, 1, -1 do
 				mp.commandv("loadfile", info.items[i], "insert-next")
 			end
@@ -26,3 +30,4 @@ mp.add_hook("on_load", 50, function()
 		mp.commandv("playlist-remove", "current")
 	end
 end)
+
